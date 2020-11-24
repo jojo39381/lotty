c7153ad7ffe079e9d0228878f556243a20431b69
Joseph Yeh <josephycyeh@gmail.com>
2020-11-23 18:15:00 -0800

    info

--- src/Product.js
+++ src/Product.js
@@ -15,7 +15,7 @@ const Product = () => {
                         <h1 style={{margin:20, fontFamily:"Lato", fontSize:50}}>Your new financial saving app!</h1>
                         <p style={{margin:20, fontFamily:"Lato", fontSize:20}}>1. Deposit money into your goal-specific accounts.</p>
                         <p style={{margin:20, fontFamily:"Lato", fontSize:20}}>2. Gain a ticket into our drawing with every $20 deposited at no cost.</p>  
-                        <p style={{margin:20, fontFamily:"Lato", fontSize:20}}>3. Earn up to 3% APY with our savings accounts.</p>
+                        <p style={{margin:20, fontFamily:"Lato", fontSize:20}}>3. Reach your financial goals sooner with us!</p>
 
                         <p style={{margin:20, fontFamily:"Lato", fontSize:30}}>Banks and Sponsors pay us so you don't have to.</p>
 
