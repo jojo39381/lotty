503302c70ed9ee3178a3dfed19626d0901efa5f3
Joseph Yeh <josephycyeh@gmail.com>
2021-04-07 18:20:25 -0700

    email

--- src/actions/save.js
+++ src/actions/save.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const saveEmail = async (email) => {
     var params = {"email":email}
-    var response = await axios.post("https://uqgtqxi7.brev.dev/api/emails", params)
+    var response = await axios.post("https://jcrdj9rb.brev.dev/api/api/emails", params)
     
     return response.status
 
