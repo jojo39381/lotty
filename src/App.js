bbae13e65c5e0505e9bf403e51ad40526f333c71
Joseph Yeh <josephycyeh@berkeley.edu>
2021-09-30 10:25:43 -0700

    ad file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import './App.css';
-import Home from './Home.js'
-import TermsOfServices from './TermsOfServices'
-import PrivacyPolicy from './PrivacyPolicy'
+import "./App.css";
+import Home from "./Home.js";
+import TermsOfServices from "./TermsOfServices";
+import PrivacyPolicy from "./PrivacyPolicy";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import textFile from "./app-ads.txt";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
-       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/terms-of-services" component={TermsOfServices}/>
-       <Route path="/privacy-policy" component={PrivacyPolicy}/>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/terms-of-services" component={TermsOfServices} />
+          <Route path="/privacy-policy" component={PrivacyPolicy} />
+          <Route path="/app-ads.txt" component={textFile} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
